4a8b8fc804f734c2b484be518af1578c6e9603c2
ivi0540 <ivi0540@gmail.com>
2024-01-21 23:03:42 +0300

    feat: done isSameLength task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -181,8 +181,13 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((itemOfArr) => {
+    if (itemOfArr.length === arr[0].length) {
+      return true;
+    }
+    return false;
+  });
 }
 
 /**
