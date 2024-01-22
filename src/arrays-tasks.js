ddadca09ceb4c85a23e97b15d06350d187b40b11
ivi0540 <ivi0540@gmail.com>
2024-01-22 22:34:17 +0300

    feat: done findCommonElements task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -612,8 +612,13 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((itemOfArr1) => {
+    if (arr2.indexOf(itemOfArr1) > -1) {
+      return true;
+    }
+    return false;
+  });
 }
 
 /**
