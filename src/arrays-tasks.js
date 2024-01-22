0c4af790b6446d22bec2730876094091842d2480
ivi0540 <ivi0540@gmail.com>
2024-01-22 16:41:09 +0300

    feat: done distinct task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -299,8 +299,10 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  // Set - коллекция уникальных значений
+  const newSet = new Set(arr);
+  return Array.from(newSet);
 }
 
 /**
